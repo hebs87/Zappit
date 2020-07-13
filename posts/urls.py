b05f7672e63549474e6fbd9a134784d4fe9f3a0f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-13 17:06:55 +0100

    Amended vote url

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('posts/', PostList.as_view()),
-    path('posts/<int:pk>', VoteCreate.as_view()),
+    path('posts/<int:pk>/vote', VoteCreate.as_view()),
 ]
