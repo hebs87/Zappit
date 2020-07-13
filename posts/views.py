bdf49c472d3d2e2585e5ca558722dd87e5031f90
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-13 16:17:53 +0100

    Created PostList view with new URLs and added to admin site

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
+
 
 # Create your views here.
+class PostList(generics.ListAPIView):
+    """
+    A view to return the Post list
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
