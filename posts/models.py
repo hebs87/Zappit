2f4d92188ef6ac7be3e16e3c3f3b39b60b2f6122
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-13 16:04:24 +0100

    Created Post and Vote models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    """
+    A model to store a user's posts
+    """
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Vote(models.Model):
+    """
+    A model to store a user's votes
+    """
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
