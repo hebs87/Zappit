1024f1365cacdd8b062e8a8e30d28907809ffed2
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-13 17:05:29 +0100

    Added functionality to display vote count for each post

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,10 +9,15 @@ class PostSerializer(serializers.ModelSerializer):
     # Enables us to automatically set the user when post is created - poster value is set in the view
     poster = serializers.ReadOnlyField(source='poster.username')
     poster_id = serializers.ReadOnlyField(source='poster.id')
+    votes = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created']
+        fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created', 'votes']
+
+    # Get the number of votes for a particular post
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()
 
 
 class VoteSerializer(serializers.ModelSerializer):
