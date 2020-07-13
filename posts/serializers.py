ce0b70172814c54a5716d3b88b1885b72e903dbd
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-13 16:37:24 +0100

    Added POST functionality to PostList view and set auth params

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,6 +6,10 @@ class PostSerializer(serializers.ModelSerializer):
     """
     A serializer to convert the Post model to JSON
     """
+    # Enables us to automatically set the user when post is created - poster value is set in the view
+    poster = serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.id')
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'poster', 'created']
+        fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created']
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import *
 from .serializers import *
 
 
 # Create your views here.
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     """
     A view to return the Post list
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # Override the standard method to pass in the poster details
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
