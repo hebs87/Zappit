66bcd3ec6b4618e216fb1fab81ad69bd855fc4d9
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-13 17:01:33 +0100

    Added functionality for user to delete their vote

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, mixins, status
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from .models import *
 from .serializers import *
@@ -9,7 +10,7 @@ from .serializers import *
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
     """
-    A view to return the Post list
+    A view to return the Post list and allow post creation
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -20,19 +21,29 @@ class PostList(generics.ListCreateAPIView):
         serializer.save(poster=self.request.user)
 
 
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
     """
-    A view to return the Post list
+    A view to allow users to vote/remove vote
+    DestroyModelMixin allows us to delete records
     """
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    # Check if user has voted on a particular post
     def get_queryset(self):
         voter = self.request.user
         post = Post.objects.get(pk=self.kwargs['pk'])
         return Vote.objects.filter(voter=voter, post=post)
 
+    # Check if user has voted on a particular post before saving vote
     def perform_create(self, serializer):
         if self.get_queryset().exists():
             raise ValidationError('You have already voted for this post :)')
         serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+    # Check if user has voted on a particular post before deleting vote
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        raise ValidationError('You never voted for this post!')
